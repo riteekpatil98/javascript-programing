bc0e3b69b8384ad90539a63a56ddbd8a5bf40569
Riteekpatil <ritspatil555@gmail.com>
2025-05-06 19:08:52 +0530

    assingnedValuetoVariable
    assingnedValuetoVariable

---
+++ 01-variableAssingn.js
@@ -0,0 +1,57 @@
+console.log("||-----------Task-First----------||");
+
+var myFullName;
+console.log("Before the value initialization  of variable myFullName is:-",myFullName);
+
+var result = typeof myFullName;
+console.log("Type of variable before initialization value :- ",result);
+
+var myFullName="Riteek Patil";
+console.log("After The value initialization  of variable myFullName is:- ",myFullName);
+var result= typeof myFullName;
+console.log("Type of variable After initialization value :-",result);
+
+var myFullName="Riteek Mohan Patil";
+console.log("Updated Full Name is :- ",myFullName);
+
+console.log("||-----------Task-Second----------||");
+
+var myLove="I love Only Java Script";
+console.log(myLove);
+console.log("||-----------Task-Third ----------||");
+
+var firstName="Riteek";
+console.log("First Name is:- ",firstName);
+
+ var lastName="Patil";
+ console.log("Last Name is:- ",lastName);
+
+ var age=24;
+ console.log("Age is :-",age);
+ var result=typeof age;
+ console.log("Type of Variable is:-",result);
+
+ var address ="At Post:- vitwe Tal. Raver Dist. Jalgaon";
+ console.log("Address:-",address);
+
+ var pinCode=425507;
+ console.log("PinCode is:-",pinCode);
+
+ var gender="Male";
+ console.log("Gender is:-",gender);
+
+ var isMarried= false;
+ console.log("Married status:- ",isMarried);
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+ 
+
+
+
+
