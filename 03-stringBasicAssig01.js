f24e387d6c72034bf6833581df58d89484facb64
Riteekpatil <ritspatil555@gmail.com>
2025-05-09 22:05:52 +0530

    03-StringBsicAssig01
    03-StringBsicAssig01

---
+++ 03-stringBasicAssig01.js
@@ -0,0 +1,14 @@
+console.log(`||----------------Today Assignment----------------||`);
+
+function stringBasics(){
+    console.log(`My dream Company Name is:- Apple`);
+     var hobby1='Learning';
+     var hobby2="Reading";
+     var hobby3="Playing Cricket";
+     console.log(`My hobby :-${hobby1},${hobby2},${hobby3}`);
+
+     var totalNumberofCharacter=hobby1.length+hobby2.length+hobby3.length;
+     console.log(`Total Number Of Character is:-${totalNumberofCharacter}`);
+           
+}
+stringBasics();
