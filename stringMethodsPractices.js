b6fb7d085b753ac7dc1654d261fef26695e4de8c
Riteekpatil <ritspatil555@gmail.com>
2025-05-10 17:14:49 +0530

    String methodes Practices
    String methodes Practices

---
+++ stringMethodsPractices.js
@@ -0,0 +1,55 @@
+var str= "  Hello guy please do work very well  ";
+var retChara=str.charAt(9);
+console.log(`${retChara}`);
+
+var con="well done";
+var concat=str.concat("keep it up").concat(con);
+console.log(`${concat}`);
+
+
+var indexof=str.indexOf("w");
+console.log(`${indexof}`);
+
+
+var lastInex=str.lastIndexOf("ell");
+console.log(`${lastInex}`);
+
+
+console.log(`${str.replace("Hello","Hi")}`);
+
+console.log(`${str.search("well")}`);
+
+
+
+console.log(`${str.slice(3,8)}`);
+
+console.log(`${str.substring(3,8)}`);
+
+console.log(`${str.substr(3,8)}`);
+
+
+console.log(`${str.toUpperCase()}`);
+
+console.log(`${str.toLowerCase()}`);
+
+var no=8329070706;
+console.log(`${no.toString()}`);
+var type=typeof no;
+console.log(typeof no.toString());
+
+// var include=
+console.log(`${str.toLowerCase().includes("H")}`);
+
+
+
+var split=str.split(" ");
+console.log(`${split}`);
+
+
+
+
+
+
+
+
+
