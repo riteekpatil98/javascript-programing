c99afad4a5c7b49c64a3cadc3f6de98425525e7a
Riteekpatil <ritspatil555@gmail.com>
2025-05-13 15:38:00 +0530

    operatorNan
    operatorNan

---
+++ 05-opertaorNan.js
@@ -0,0 +1,7 @@
+console.log(`====== NaN =======`);
+console.log(0/0);
+console.log(`==== String to Number conversion =======`);
+
+var num = "Riteek";
+var result = +num;
+console.log(` Value : ${result} and it's type :${typeof result}`);
